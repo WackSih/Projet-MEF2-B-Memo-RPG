5911ae974ef6df5326c95828f4d12095c3006234
elliotmnr <elliot.meunier6@gmail.com>
2026-04-21 11:42:51 +0200

    Update structure.c

--- structure.c
+++ structure.c
@@ -23,7 +23,7 @@ typedef struct {
     Type_case type;
     Arme arme_choisie;
     int est_decouverte;
-    int proprietaire; // 0 si ce n'est pas un trésor perso, {1,2,3,4} selon le joueur dont c'est le trésor
+    int proprietaire; // -1 si ce n'est pas un trésor perso, {0,1,2,3} selon le joueur dont c'est le trésor
 }Case;
 
 typedef struct {
