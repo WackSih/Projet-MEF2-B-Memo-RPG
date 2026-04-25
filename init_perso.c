958bfab891ff6a17c5c46a3c143fd792c8961064
kant1-bls <quentin.blaise99@gmail.com>
2026-04-25 11:44:56 +0200

    Update init_perso.c

--- init_perso.c
+++ init_perso.c
@@ -1,29 +1,29 @@
-#include"memo_rpg.h"
+#include "memo_rpg.h"
 
-void depart(Personnage tab_joueurs[], int nb_joueurs) { //Initialise la position des perso sur la map en fct du nb de joueurs
+void depart(Personnage tab_joueurs[], int nb_joueurs) { 
     for (int i = 0; i < nb_joueurs; i++) {
         tab_joueurs[i].aLarme = 0;
         tab_joueurs[i].aLeTresor = 0;
-        tab_joueurs[i].arme_actuelle = EPEE; // Arme par défaut
+        tab_joueurs[i].arme_actuelle = EPEE; 
 
-        if (i == 0) { // Nord
+        if (i == 0) { 
             tab_joueurs[i].ligne = -1; 
             tab_joueurs[i].colonne = 2;
         }
         else if (i == 1) { 
-            if (nb_joueurs == 2) { // Face à face si il y a que 2 joueurs
+            if (nb_joueurs == 2) { 
                 tab_joueurs[i].ligne = 5; 
                 tab_joueurs[i].colonne = 2; 
-            } else { // Est
+            } else { 
                 tab_joueurs[i].ligne = 2;
                 tab_joueurs[i].colonne = 5;
             }
         }
-        else if (i == 2) { // Sud
+        else if (i == 2) { 
             tab_joueurs[i].ligne = 5;
             tab_joueurs[i].colonne = 2;
         }
-        else if (i == 3) { // Ouest
+        else if (i == 3) { 
             tab_joueurs[i].ligne = 2;
             tab_joueurs[i].colonne = -1;
         }
@@ -32,7 +32,7 @@ void depart(Personnage tab_joueurs[], int nb_joueurs) { //Initialise la position
 
 void select_perso(Personnage tab_joueurs[], int nb_joueurs) {
     int choix = 0;
-	int deja_pris[4] = {0, 0, 0, 0};
+    int deja_pris[4] = {0, 0, 0, 0};
     for (int i = 0; i < nb_joueurs; i++) {
         printf("\nJoueur %d, choisissez votre personnage :\n", i + 1);
         printf("1: GANDALF\n2: TAURIEL\n3: GOLLUM\n4: GIMLI\nChoix : ");
@@ -43,23 +43,27 @@ void select_perso(Personnage tab_joueurs[], int nb_joueurs) {
             }
         } while(choix < 1 || choix > 4 || deja_pris[choix-1] == 1);
 
-        tab_joueurs[i].perso = (Classe)(choix-1); //Mise à jour de la structure des joueurs
+        tab_joueurs[i].perso = (Classe)(choix-1); 
         
         if (choix == 1){ 
-			strcpy(tab_joueurs[i].nomJoueur, "Gandalf");
-			tab_joueurs[i].arme_a_trouver = BATON; // ARME_SPE de Gandalf
-			deja_pris[0] = 1;}
+            strcpy(tab_joueurs[i].nomJoueur, "Gandalf");
+            tab_joueurs[i].arme_a_trouver = BATON; 
+            deja_pris[0] = 1;
+        }
         else if (choix == 2){
-			strcpy(tab_joueurs[i].nomJoueur, "Tauriel");
-			tab_joueurs[i].arme_a_trouver = ARC_M; // ARME_SPE de Tauriel
-			deja_pris[1] = 1;}
+            strcpy(tab_joueurs[i].nomJoueur, "Tauriel");
+            tab_joueurs[i].arme_a_trouver = ARC_M; 
+            deja_pris[1] = 1;
+        }
         else if (choix == 3){
-			strcpy(tab_joueurs[i].nomJoueur, "Gollum");
-			tab_joueurs[i].arme_a_trouver = ANNEAU; // ARME_SPE de Gollum
-			deja_pris[2] = 1;}
+            strcpy(tab_joueurs[i].nomJoueur, "Gollum");
+            tab_joueurs[i].arme_a_trouver = ANNEAU; 
+            deja_pris[2] = 1;
+        }
         else if (choix == 4){
-			strcpy(tab_joueurs[i].nomJoueur, "Gimli");
-			tab_joueurs[i].arme_a_trouver = HACHE; // ARME_SPE de Gimli
-			deja_pris[3] = 1;}
+            strcpy(tab_joueurs[i].nomJoueur, "Gimli");
+            tab_joueurs[i].arme_a_trouver = HACHE; 
+            deja_pris[3] = 1;
+        }
     }
 }
