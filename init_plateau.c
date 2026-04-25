c873407f201a040ba9e5a6f7b2e24292f3e22000
elliotmnr <elliot.meunier6@gmail.com>
2026-04-25 09:56:12 +0200

    Update init_plateau.c

--- init_plateau.c
+++ init_plateau.c
@@ -26,10 +26,10 @@ Plateau initialisation(){
     liste[16].type = TRESOR; liste[16].proprietaire = -1;
     liste[17].type = TRESOR; liste[17].proprietaire = -1;
 
-    liste[18].type = TRESOR_PERSO; liste[18].proprietaire = 0; // Joueur 1
-    liste[19].type = TRESOR_PERSO; liste[19].proprietaire = 1; // Joueur 2
-    liste[20].type = TRESOR_PERSO; liste[20].proprietaire = 2; // Joueur 3
-    liste[21].type = TRESOR_PERSO; liste[21].proprietaire = 3; // Joueur 4
+    liste[18].type = ARME_SPE; liste[18].proprietaire = 0; // Joueur 1
+    liste[19].type = ARME_SPE; liste[19].proprietaire = 1; // Joueur 2
+    liste[20].type = ARME_SPE; liste[20].proprietaire = 2; // Joueur 3
+    liste[21].type = ARME_SPE; liste[21].proprietaire = 3; // Joueur 4
 
     liste[22].type = PORTAIL; liste[22].proprietaire = -1;
     liste[23].type = TOTEM; liste[23].proprietaire = -1;
