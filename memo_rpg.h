2b30f417b3324c23ceea810823f213dcaf4535f3
kant1-bls <quentin.blaise99@gmail.com>
2026-04-28 10:17:20 +0200

    Update memo_rpg.h

--- memo_rpg.h
+++ memo_rpg.h
@@ -11,7 +11,7 @@
 // Enums
 typedef enum { GANDALF, TAURIEL, GOLLUM, GIMLI } Classe;
 typedef enum { BATON, ARC_M, ANNEAU, HACHE } Arme_spe;
-typedef enum {VIDE, DRAGON, ORC, NAZGUL, ARAIGNE, PORTAIL, TOTEM, TRESOR, ARME_SPE, ARAIGNEE} Type_case;
+typedef enum {VIDE, DRAGON, ORC, NAZGUL, ARAIGNEE, PORTAIL, TOTEM, TRESOR, ARME_SPE, DEPART} Type_case;
 typedef enum { ARC, EPEE, BDF, LANCE } Arme;
 
 // Structures
@@ -44,17 +44,18 @@ typedef struct {
 } StatJoueur;
 
 // Prototypes (Indispensables pour lier les fichiers)
-Plateau initialisation();
+Plateau initialisation(Personnage tab_joueurs[], int nb_joueurs);
 void depart(Personnage tab_joueurs[], int nb_joueurs);
 void select_perso(Personnage tab_joueurs[], int nb_joueurs);
 Personnage* choix_arme(Personnage *x);
 int verifcaselibre(Plateau* tab, int colonne_new, int ligne_new);
 void deplacement(Personnage* x, Plateau* tab);
-void resolution_case(Personnage* x, Plateau* tab);
+int resolution_case(Personnage* x, Plateau* tab);
 void afficher_plateau(Plateau* p, Personnage* joueurs, int nb_joueurs);
 void deroulement_jeu(Plateau* tab, Personnage* joueur, int nb_joueurs);
 void reset_tableau(Personnage* x, Plateau* tab);
 int charger_stats(StatJoueur tableau[], int max_joueurs);
 int sauvegarder_stats(StatJoueur tableau[], int nb_joueurs);
+void mettre_a_jour_stats(const char* nom, int a_gagne);
 
 #endif
