6198bc93a21fd72d1cc71b70ce85916e71d24233
elliotmnr <elliot.meunier6@gmail.com>
2026-05-06 09:05:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,13 +7,13 @@ int main() {
     while (continuer != 0) {
         Personnage joueurs[4];
         int nb_joueurs;
+        int menu;
         do{
         printf("\n=== BIENVENUE DANS MEMO-RPG ===\n");
         printf("1. Jouer une partie\n");
         printf("2. Voir le Hall of Fame\n");
         printf("0. Quitter\n");
         printf("Votre choix : ");
-        int menu;
         scanf("%d", &menu);
         if(menu<0 || menu > 2){
             printf("Erreur, veuillez selectionner a nouveau.\n");
