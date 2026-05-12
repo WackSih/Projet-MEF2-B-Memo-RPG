97208f086a7326c4eb1782ae126daeb0bcd1e9ff
kant1-bls <quentin.blaise99@gmail.com>
2026-05-12 09:51:15 +0200

    Changements commentaires

--- init_plateau.c
+++ init_plateau.c
@@ -30,14 +30,14 @@ Plateau initialisation(Personnage tab_joueurs[], int nb_joueur) {
         } 
         else if (tab_joueurs[i].perso == GIMLI) {
             liste[compteur].proprietaire = 3;
-        } // On lie l'arme au perso
+        } // ON AFFECTE CHAQUE PERSO A SON ARME
         compteur++;
     }
 
     // REMPLISSAGE DU RESTE AVEC DES MONSTRES
-    // S'il manque des cases pour arriver à 25 (ex: partie à 2 ou 3 joueurs)
+    // S'IL Y A MOINS DE 4 JOUEURS
     while(compteur < 25) {
-        int monstre_aleatoire = rand() % 4; // Tire un chiffre entre 0 et 3
+        int monstre_aleatoire = rand() % 4; // TIRE UN CHIFFRE ENTRE 0 ET 3
         
         if (monstre_aleatoire == 0) liste[compteur].type = DRAGON;
         else if (monstre_aleatoire == 1) liste[compteur].type = ORC;
@@ -47,7 +47,7 @@ Plateau initialisation(Personnage tab_joueurs[], int nb_joueur) {
         compteur++;
     }
 
-    // Mélange Parfait Fisher-Yates
+    // MELANGE PARFAIT FISHER-YATES
     for(i = 0; i < 25; i++){
         liste[i].est_decouverte = 0;
     }
@@ -61,18 +61,18 @@ Plateau initialisation(Personnage tab_joueurs[], int nb_joueur) {
     int index_liste = 0;
     for(int lig = 0; lig < TAILLE; lig++) {
         for(int col = 0; col < TAILLE; col++) {
-            // Carré central
+            // CARRE CENTRAL
             if (lig >= 1 && lig <= 5 && col >= 1 && col <= 5) {
                 p.tableau[lig][col] = liste[index_liste++];
             } 
-            // Spawn joueurs
+            // SPAWN JOUEURS
             else if ((lig == 0 && col == 3) || (lig == 3 && col == 0) || 
                      (lig == 6 && col == 3) || (lig == 3 && col == 6)) {
                 p.tableau[lig][col].type = DEPART; // Laisse ça en PORTAIL ou VIDE, c'est juste le visuel du spawn
                 p.tableau[lig][col].est_decouverte = 1;
                 p.tableau[lig][col].proprietaire = -1;
             } 
-            // Bords inutilisés
+            // BORDURES DU PLATEAU (INUTILISEES)
             else {
                 p.tableau[lig][col].type = VIDE;
                 p.tableau[lig][col].est_decouverte = 1;
